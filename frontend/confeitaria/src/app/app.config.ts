83f546bab091c5d4f4b3fc42d7098d0a46e69dbe
Sthefany Evangelista <sthefanyematias@gmail.com>
2026-06-17 14:55:09 -0300

    fix: ativa a estrategia de hash location para corrigir 404 em subrotas estaticas

--- frontend/confeitaria/src/app/app.config.ts
+++ frontend/confeitaria/src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withInMemoryScrolling } from '@angular/router';
+// import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { provideRouter, withInMemoryScrolling, withHashLocation } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
@@ -8,8 +9,13 @@ import { apiInterceptor } from './core/services/api-config.service';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
+    // provideRouter(
+    //   routes,
+    //   withInMemoryScrolling({ scrollPositionRestoration: 'top' })
+    // ),
     provideRouter(
       routes,
+      withHashLocation(),
       withInMemoryScrolling({ scrollPositionRestoration: 'top' })
     ),
     provideHttpClient(
