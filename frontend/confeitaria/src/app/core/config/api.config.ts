81a6ab22c63f048badcb372c8dc2795bb79aaa21
Sthefany Evangelista <sthefanyematias@gmail.com>
2026-06-17 13:52:28 -0300

    feat: atualiza url do backend para producao

--- frontend/confeitaria/src/app/core/config/api.config.ts
+++ frontend/confeitaria/src/app/core/config/api.config.ts
@@ -5,5 +5,6 @@ export const API_ATIVA: ApiTipo = 'spring';
 
 export const API_URLS: Record<ApiTipo, string> = {
     manual: 'http://localhost:3000',
-    spring: 'http://localhost:8080'
+    // spring: 'http://localhost:8080'
+    spring: 'https://confeitaria-backend-zzm3.onrender.com'
 };
